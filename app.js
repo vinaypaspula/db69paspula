d2570f5e2e7eacf2621237801822b7ded69461fc
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-04-06 20:50:52 -0500

    added database string

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var books = require("./models/books");
+
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true, useUnifiedTopology: true});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
