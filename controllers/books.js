e4178fcee71e5c29a7c37b5b1d0dd617b35390bb
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-04-07 18:14:19 -0500

    added books.js

--- controllers/books.js
+++ controllers/books.js
@@ -1,21 +1,29 @@
 var books = require('../models/books');
-// List of all Costumes
-exports.books_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Books list');
-};
-// for a specific Costume.
+// List of all books
+exports.books_list = async function(req, res) {
+    try{
+    thebooks = await books.find();
+    res.send(thebooks);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
+//res.send('NOT IMPLEMENTED: Books list');
+
+// for a specific books.
 exports.books_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: books detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle books create on POST.
 exports.books_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: books create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle books delete form on DELETE.
 exports.books_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle books update form on PUT.
 exports.books_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
 };
