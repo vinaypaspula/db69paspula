3584bfa1a39a6681d6d229fa8a1ea4870ebbac4f
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-04-07 19:28:21 -0500

    Done

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,9 @@
-var books = require('../models/books');
+var books = require('../models/books')
 // List of all books
+exports.books_list = async function (req, res) {
+    // List of all books
+    res.send('NOT IMPLEMENTED: books list');
+};
 exports.books_list = async function(req, res) {
     try{
     thebooks = await books.find();
@@ -9,30 +13,43 @@ exports.books_list = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
-//res.send('NOT IMPLEMENTED: Books list');
-
-// for a specific books.
-exports.books_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: books detail: ' + req.params.id);
+// for a specific Handbag.
+exports.books_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Handbag detail: ' + req.params.id);
 };
-// Handle books create on POST.
-exports.books_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: books create POST');
-};
+// Handle Handbag create on POST.
+exports.books_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new books();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"Dimensions":"Stylish", "Color":"Violet", "Price":1500};
+    document.name = req.body.name;
+    document.author = req.body.author;
+    document.price = req.body.price;
+    try{
+        let result = await document.save();
+        res.send(result);
+        }
+        catch(err){
+        res.error(500,`{"error": ${err}}`);
+        }
+    };
 // Handle books delete form on DELETE.
-exports.books_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
+exports.books_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Handbag delete DELETE ' + req.params.id);
 };
 // Handle books update form on PUT.
-exports.books_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
+exports.books_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
 };
 // VIEWS
 // Handle a show all view
 exports.books_view_all_Page = async function(req, res) {
     try{
     thebooks = await books.find();
-    res.render('books', { title: 'books Search Results', results: thebooks });
+    res.render('books', { title: 'Handbag Search Results', results: thebooks });
     }
     catch(err){
     res.error(500,`{"error": ${err}}`);
