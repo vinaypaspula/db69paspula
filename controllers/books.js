4fd5e26b6f19625e389b8ee5aa89827335bf6b15
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-04-09 13:14:40 -0500

    Assignment 12

--- controllers/books.js
+++ controllers/books.js
@@ -14,8 +14,15 @@ exports.books_list = async function(req, res) {
     }
     };
 // for a specific Handbag.
-exports.books_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Handbag detail: ' + req.params.id);
+exports.books_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await books.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle Handbag create on POST.
 exports.books_create_post = async function (req, res) {
@@ -41,8 +48,21 @@ exports.books_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Handbag delete DELETE ' + req.params.id);
 };
 // Handle books update form on PUT.
-exports.books_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
+exports.books_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await books.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.name) toUpdate.name = req.body.name;
+        if(req.body.author) toUpdate.author = req.body.author;
+        if(req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 // VIEWS
 // Handle a show all view
--- routes/resource.js
+++ routes/resource.js
@@ -6,15 +6,15 @@ var books_controller = require('../controllers/books');
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
-/// Handbag ROUTES ///
-// POST request for creating a Handbag.
+/// books ROUTES ///
+// POST request for creating a books.
 router.post('/books', books_controller.books_create_post);
-// DELETE request to delete Handbag.
+// DELETE request to delete books.
 router.delete('/books/:id', books_controller.books_delete);
-// PUT request to update Handbag.
+// PUT request to update books.
 router.put('/books/:id', books_controller.books_update_put);
-// GET request for one Handbag.
+// GET request for one books.
 router.get('/books/:id', books_controller.books_detail);
-// GET request for list of all Handbag.
+// GET request for list of all books.
 router.get('/books', books_controller.books_list);
 module.exports = router;
